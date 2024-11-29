5d63985d6365018ff45c5398ca73d41bfd5a04f6
David <davidsandoval0051@gmail.com>
2024-11-29 03:21:55 -0600

    [feature]: server fixed

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,21 +1,21 @@
 import { CheckService } from "../domain/use-cases/checks/check-service";
+import { SendEmailLogs } from "../domain/use-cases/email/send-email-logs";
 import { FileSystemDataSource } from "../infrastructure/datasources/file-system.datasource";
 import { LogRepositoryImpl } from "../infrastructure/respositories/log.repository.impl";
 import { CronService } from "./cron/cron-service";
 import { EmailService } from "./email/email.service";
 
 const fileSystemLogRepository = new LogRepositoryImpl(new FileSystemDataSource());
+const emailService = new EmailService();
 
 export class Server {
   public static start() {
     console.log('Server started');
 
+    new SendEmailLogs(emailService, fileSystemLogRepository)
+      .execute(['davidsandoval20051@gmail.com']);
 
-    const emailService = new EmailService(
-      fileSystemLogRepository
-    );
-
-    emailService.sendEmailWithFileSystemLog(['davidsandoval20051@gmail.com', 'okrunate@gmail.com']);
+    // emailService.sendEmailWithFileSystemLog(['davidsandoval20051@gmail.com', 'okrunate@gmail.com']);
 
 
     // CronService.createJob('*/5 * * * * *', () => {
